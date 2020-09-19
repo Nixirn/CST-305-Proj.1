3d7bd8fff4c2b253e6ca4f18bf4fe049bdcd2c82
Nixirn <70537048+Nixirn@users.noreply.github.com>
2020-09-19 12:52:21 -0700

    Add files via upload
    Final Code

--- Project1.py
+++ Project1.py
@@ -1,38 +1,32 @@
-
-#import the required modules
 import numpy as np
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-from scipy.integrate import odeint
+# function that returns dx/dt
 
-def model(x, t, k):
-#    k = 84 #define a constant, can be commented out for args
-    dydt = 1/k #the differential equation
-    return dydt
+def model(y,t,p):
+    dpdt = y / p
+    return dpdt
 
-#initial condition
-y0 = 0
+# initial condition
+y0 = 125000000
 
-#time points
-t= np.linspace(0,20)
+# time points
+t = np.linspace(0,500)
 
-#solve ODEs
-k=84
-y1 = odeint(model, y0, t, args=(k,)) #function that solves the function described above
-k = 88
-y2 = odeint(model, y0, t, args=(k,))
-k=92
-y3 = odeint(model,y0,t, args=(k,))
+# solve ODE
+p = 84
+y1 = odeint(model,y0,t,args=(p,))
+p = 88
+y2 = odeint(model,y0,t,args=(p,))
+p = 92
+y3 = odeint(model,y0,t,args=(p,))
 
-#get or set the limits of the independent variable, speed
-#plt.xlim(1250000,125000000)
-#plt.ylim(14000,1400000)
-#plot results
-plt.plot(t,y1, 'r-',linewidth=2,label='k=84 bytes per packet')
-plt.plot(t,y2,'b-',linewidth=2,label='k=88 bytes per packet')
-plt.plot(t,y3,'g:',linewidth=2,label='k=92 bytes per packet')
-plt.xlabel('speed (bytes/second)')
-plt.ylabel('y(t) packets per second')
+# plot results
+plt.plot(t,y1,'r-',linewidth=2,label='p = 84')
+plt.plot(t,y2,'b--',linewidth=2,label='p = 88 ')
+plt.plot(t,y3,'g:',linewidth=2,label='p = 92')
+plt.xlabel('time')
+plt.ylabel('p(t)')
 plt.legend()
 plt.show()
-
